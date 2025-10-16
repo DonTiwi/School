2caae503c26112b9020ef006ea15fb94567f5307
Eirik <eirikgustafson@gmail.com>
2025-10-16 12:42:28 +0200

    Updated

--- WeeklyAssignments/week42.mjs
+++ WeeklyAssignments/week42.mjs
@@ -20,3 +20,24 @@ function insertAt(arr, index, value) {
 }
 
 console.log(insertAt(numArr, 3, 1000));
+
+function sliceManual(arr, start, end) {
+  if (start < 0 || start >= arr.length) {
+    return [];
+  }
+
+  if (end == undefined || end >= arr.lenght || end < start) {
+    end = arr.lenght - 1;
+  }
+
+  let output = [];
+
+  for (let i = start; i <= end; i++) {
+    output.push(arr[i]);
+  }
+
+  return output;
+}
+
+let slice = sliceManual(numArr, 2);
+console.log(slice);
