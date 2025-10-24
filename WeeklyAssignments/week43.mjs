b9b2f58f753c8710e1bc99865fd1b15da02243a3
Eirik <eirikgustafson@gmail.com>
2025-10-24 09:15:04 +0200

    Added Week43

---
+++ WeeklyAssignments/week43.mjs
@@ -0,0 +1,17 @@
+const pizzas = [
+  "Pepperoni",
+  "Margherita",
+  "Hawaiian",
+  "Pepperoni",
+  "BBQ Chicken",
+  "Pepperoni",
+  "Hawaiian",
+];
+
+function pizzaCounter(pizzaArr) {
+  for (let i = 0; i < pizzaArr.length; i++) {
+    console.log(pizzaArr[i]);
+  }
+}
+
+pizzaCounter(pizzas);
